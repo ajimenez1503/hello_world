6568382efb3e0f0b923b4cfa27703a0d241ecf05
Antonio Jimenez <softwarejimenezz@gmail.com>
2019-04-04 10:15:25 +0200

    Add test for srcCpp

--- src/src_cpp/test/test.cpp
+++ src/src_cpp/test/test.cpp
@@ -1,25 +1,21 @@
 
 
 #include <iostream>
-#include <stdexcept>
-#include <string>
-#include "hello_world_cpp.cpp"
+
 
 
 #define ASSERT_EQUAL_INT( x, y )                                    \
 {                                                                   \
-  if( ( (int)x ) != ( (int)y ) )                                              \
+  if( ( (int)x ) != ( (int)y ) )                                    \
   {                                                                 \
-    throw std::runtime_error(   std::string( __FILE__ )             \
-                              + std::string( ":" )                  \
-                              + std::to_string( __LINE__ )          \
-                              + std::string( " in " )               \
-                              + std::string( __PRETTY_FUNCTION__ )  \
-                              + std::string( ": " )                 \
-                              + std::to_string( ( x ) )             \
-                              + std::string( " != " )               \
-                              + std::to_string( ( y ) )             \
-    );                                                              \
+        std::cout << "ASSERT_EQUAL_INT Failed "                     \
+                <<  __PRETTY_FUNCTION__                             \
+                << " in "                                           \
+                << __FILE__                                         \
+                << ":"                                              \
+                << __LINE__                                         \
+                << std::endl;                                       \
+        return false;                                               \
   }                                                                 \
 }
 
@@ -28,11 +24,74 @@
 {                                                                   \
   if( !( condition ) )                                              \
   {                                                                 \
-    throw std::runtime_error(   std::string( __FILE__ )             \
-                              + std::string( ":" )                  \
-                              + std::to_string( __LINE__ )          \
-                              + std::string( " in " )               \
-                              + std::string( __PRETTY_FUNCTION__ )  \
-    );                                                              \
+        std::cout << "ASSERT_TRUE Failed "                          \
+                <<  __PRETTY_FUNCTION__                             \
+                << " in "                                           \
+                << __FILE__                                         \
+                << ":"                                              \
+                << __LINE__                                         \
+                << std::endl;                                       \
+        return false;                                               \
   }                                                                 \
+}
+
+#define ASSERT_FALSE( condition )                                   \
+{                                                                   \
+  if( ( condition ) )                                               \
+  {                                                                 \
+        std::cout << "ASSERT_FALSE Failed "                         \
+                <<  __PRETTY_FUNCTION__                             \
+                << " in "                                           \
+                << __FILE__                                         \
+                << ":"                                              \
+                << __LINE__                                         \
+                << std::endl;                                       \
+        return false;                                               \
+  }                                                                 \
+}
+
+#define ASSERT_RETCODE( condition )                                 \
+{                                                                   \
+  if( !( condition ) )                                              \
+  {                                                                 \
+        return 1;                                                   \
+  }                                                                 \
+}
+
+
+
+#define PRINT_TEST_NAME()                                           \
+{                                                                   \
+  std::cout << "Running: "  <<  __FUNCTION__  << std::endl;         \
+}
+
+
+bool test1() {
+    PRINT_TEST_NAME();
+    ASSERT_TRUE(true);
+
+    return true;
+}
+
+bool test2() {
+    PRINT_TEST_NAME();
+    ASSERT_FALSE(false);
+
+    return true;
+}
+
+bool test3() {
+    PRINT_TEST_NAME();
+    ASSERT_EQUAL_INT(1,1);
+
+    return true;
+}
+
+int main() {
+     std::cout << "Start Tests" <<  std::endl; 
+    ASSERT_RETCODE(test1());
+    ASSERT_RETCODE(test2());
+    ASSERT_RETCODE(test3());
+
+    return 0;
 }
